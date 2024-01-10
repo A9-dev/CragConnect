e106533b10c936e588626a7231bd046407df1343
henry <henrypearson422@gmail.com>
2024-01-10 13:31:27 +0000

    Profile button basic

--- client/src/ProfileButton.jsx
+++ client/src/ProfileButton.jsx
@@ -5,43 +5,75 @@ import {
   MenuList,
   MenuItem,
   useColorModeValue,
+  Box,
+  Heading,
+  Center,
+} from "@chakra-ui/react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  Text,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
 } from "@chakra-ui/react";
 import { AtSignIcon } from "@chakra-ui/icons";
 import { useContext } from "react";
 import { AppContext } from "./App";
 
 const ProfileButton = () => {
-  const { setLoggedIn, setUsername, username } = useContext(AppContext);
+  const { setLoggedIn, setUsername, username, subscriptions } =
+    useContext(AppContext);
   const isDarkMode = useColorModeValue(false, true);
 
   const handleSignout = () => {
     setLoggedIn(false);
     setUsername("");
   };
-
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const handleProfile = () => {
-    console.log("Profile clicked");
+    onOpen();
     // TODO: Add functionality to profile button
   };
 
   return (
-    <Menu>
-      <MenuButton
-        as={Button}
-        leftIcon={<AtSignIcon />}
-        bg={isDarkMode ? "gray.800" : "gray.200"}
-        color={isDarkMode ? "white" : "black"}
-      >
-        {username}
-      </MenuButton>
-      <MenuList
-        bg={isDarkMode ? "gray.800" : "gray.200"}
-        color={isDarkMode ? "white" : "black"}
-      >
-        <MenuItem onClick={handleProfile}>Profile</MenuItem>
-        <MenuItem onClick={handleSignout}>Sign out</MenuItem>
-      </MenuList>
-    </Menu>
+    <Box>
+      <Menu>
+        <MenuButton
+          as={Button}
+          leftIcon={<AtSignIcon />}
+          bg={isDarkMode ? "gray.800" : "gray.200"}
+          color={isDarkMode ? "white" : "black"}
+        >
+          {username}
+        </MenuButton>
+        <MenuList
+          bg={isDarkMode ? "gray.800" : "gray.200"}
+          color={isDarkMode ? "white" : "black"}
+        >
+          <MenuItem onClick={handleProfile}>Profile</MenuItem>
+          <MenuItem onClick={handleSignout}>Sign out</MenuItem>
+        </MenuList>
+      </Menu>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent bg={isDarkMode ? "gray.800" : "gray.200"}>
+          <ModalHeader>Profile</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Center>
+              <Heading>{username}</Heading>
+            </Center>
+            <Text>Following:</Text>
+            {subscriptions.map((subscription) => {
+              return <Text key={subscription}>{subscription}</Text>;
+            })}
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </Box>
   );
 };
 
