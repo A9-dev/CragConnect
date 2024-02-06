34c27fa840fa6a60e770a1bb214ed3e530a8a65c
A9-dev <henrypearson422@gmail.com>
2024-02-06 12:13:39 +0000

    Clean up

--- client/src/routes/Events.jsx
+++ client/src/routes/Events.jsx
@@ -1,11 +1,7 @@
-import { AppContext } from "../App";
-import CreateEventButton from "../CreateEventButton";
 import EventList from "../EventList";
-import { useContext } from "react";
 import { Box } from "@chakra-ui/react";
 
 const Events = () => {
-  const { isOrganisation } = useContext(AppContext);
   return (
     <Box width={"50%"} margin={"auto"} p={5}>
       <EventList />
