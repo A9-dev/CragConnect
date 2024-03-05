127ff64aabe5f22169844ebe0f54a6ff7a5c4cc1
A9-dev <henrypearson422@gmail.com>
2024-03-05 12:12:57 +0000

    Start on events testing

--- client/src/routes/Events.jsx
+++ client/src/routes/Events.jsx
@@ -3,7 +3,7 @@ import { Box } from "@chakra-ui/react";
 
 const Events = () => {
   return (
-    <Box width={"50%"} margin={"auto"} p={5}>
+    <Box width={"50%"} margin={"auto"} p={5} data-testid="events-page">
       <EventList />
     </Box>
   );
