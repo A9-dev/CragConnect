423f5a5f4241ca6cb2ea00d85ff1da8e13a3063a
henry <henrypearson422@gmail.com>
2024-03-27 16:39:20 +0000

    Running from just server keeping test functionality

--- client/src/dbFunctions.js
+++ client/src/dbFunctions.js
@@ -1,9 +1,14 @@
 import axios from "axios";
 
+const axiosClient = axios.create({
+  baseURL: "http://localhost:5000",
+  headers: {},
+});
+
 // Sends login request to server
 const loginUser = async (username, password) => {
   try {
-    const response = await axios.post("/login", {
+    const response = await axiosClient.post("/login", {
       username,
       password,
     });
@@ -19,7 +24,7 @@ const loginUser = async (username, password) => {
 // Sends registration request to server
 const uploadUser = async (username, password, orgBool, fullName) => {
   try {
-    const response = await axios.post("/register", {
+    const response = await axiosClient.post("/register", {
       username,
       password,
       organisation: orgBool,
@@ -37,7 +42,7 @@ const uploadUser = async (username, password, orgBool, fullName) => {
 // Fetches posts from server
 const getPosts = async () => {
   try {
-    const response = await axios.get("/posts");
+    const response = await axiosClient.get("/posts");
     // console.log(response.data);
     // console.log(response.status);
     return { data: response.data, status: response.status };
@@ -50,7 +55,7 @@ const getPosts = async () => {
 // Sends post to server
 const uploadPost = async (username, title, content) => {
   try {
-    const response = await axios.post("/posts", {
+    const response = await axiosClient.post("/posts", {
       title,
       content,
       username,
@@ -67,7 +72,7 @@ const uploadPost = async (username, title, content) => {
 // Fetches news posts from server
 const getNewsPosts = async () => {
   try {
-    const response = await axios.get("/newsPosts");
+    const response = await axiosClient.get("/newsPosts");
     // console.log(response.data);
     // console.log(response.status);
     return { data: response.data, status: response.status };
@@ -80,7 +85,7 @@ const getNewsPosts = async () => {
 // Sends news post to server
 const uploadNewsPost = async (username, title, content) => {
   try {
-    const response = await axios.post("/newsPosts", {
+    const response = await axiosClient.post("/newsPosts", {
       title,
       content,
       username,
@@ -97,7 +102,7 @@ const uploadNewsPost = async (username, title, content) => {
 // Sends subscription request to server
 const subscribe = async (subscriberUsername, subscribeeUsername) => {
   try {
-    const response = await axios.post("/subscriptions", {
+    const response = await axiosClient.post("/subscriptions", {
       username: subscriberUsername,
       subscription: subscribeeUsername,
     });
@@ -113,7 +118,7 @@ const subscribe = async (subscriberUsername, subscribeeUsername) => {
 // Sends unsubscription request to server
 const unsubscribe = async (subscriberUsername, subscribeeUsername) => {
   try {
-    const response = await axios.delete("/subscriptions", {
+    const response = await axiosClient.delete("/subscriptions", {
       data: {
         username: subscriberUsername,
         subscription: subscribeeUsername,
@@ -131,7 +136,7 @@ const unsubscribe = async (subscriberUsername, subscribeeUsername) => {
 // Sends search request to server
 const searchUser = async (searchTerm) => {
   try {
-    const response = await axios.get("/search/" + searchTerm);
+    const response = await axiosClient.get("/search/" + searchTerm);
     // console.log(response.data);
     // console.log(response.status);
     return { data: response.data, status: response.status };
@@ -168,7 +173,7 @@ const postEvent = async (
       }
     });
 
-    const response = await axios.post("/eventPosts", eventData);
+    const response = await axiosClient.post("/eventPosts", eventData);
     // console.log(response.data);
     // console.log(response.status);
     return { data: response.data, status: response.status };
@@ -180,7 +185,7 @@ const postEvent = async (
 // Fetches events from server
 const getEvents = async () => {
   try {
-    const response = await axios.get("/eventPosts");
+    const response = await axiosClient.get("/eventPosts");
     // console.log(response.data);
     // console.log(response.status);
     return { data: response.data, status: response.status };
@@ -192,7 +197,7 @@ const getEvents = async () => {
 // Deletes post from server
 const deletePost = async (postId) => {
   try {
-    const response = await axios.delete("/posts/" + postId);
+    const response = await axiosClient.delete("/posts/" + postId);
     // console.log(response.data);
     // console.log(response.status);
     return { data: response.data, status: response.status };
@@ -204,7 +209,7 @@ const deletePost = async (postId) => {
 // Fetches user data from server
 const getUserData = async (username) => {
   try {
-    const response = await axios.get("/user/" + username);
+    const response = await axiosClient.get("/user/" + username);
     // console.log(response.data);
     // console.log(response.status);
     return { data: response.data, status: response.status };
@@ -217,7 +222,7 @@ const getUserData = async (username) => {
 const updateUserData = async (userData) => {
   // console.log("From update user data:", userData);
   try {
-    const response = await axios.put(
+    const response = await axiosClient.put(
       "/user/" + userData.username,
 
       {
@@ -235,7 +240,7 @@ const updateUserData = async (userData) => {
 // Increases user's fitness score on server
 const increaseFitnessScore = async (username) => {
   try {
-    const response = await axios.put("/user/fitnessScore/" + username);
+    const response = await axiosClient.put("/user/fitnessScore/" + username);
     // console.log(response.data);
     // console.log(response.status);
     return { data: response.data, status: response.status };
@@ -248,7 +253,7 @@ const increaseFitnessScore = async (username) => {
 // Fetches top ten fitness scores from server
 const getTopTenFitnessScores = async () => {
   try {
-    const response = await axios.get("/fitnessScores/10");
+    const response = await axiosClient.get("/fitnessScores/10");
     // console.log(response.data);
     // console.log(response.status);
     return { data: response.data, status: response.status };
@@ -260,7 +265,9 @@ const getTopTenFitnessScores = async () => {
 // Resets user's exercises done on server
 const resetExercisesDone = async (username) => {
   try {
-    const response = await axios.put("/user/resetExercisesDone/" + username);
+    const response = await axiosClient.put(
+      "/user/resetExercisesDone/" + username
+    );
     // console.log(response.data);
     // console.log(response.status);
     return { data: response.data, status: response.status };
@@ -273,9 +280,12 @@ const resetExercisesDone = async (username) => {
 // Sets user's exercises done on server
 const setExercisesDoneDB = async (username, exercisesDone) => {
   try {
-    const response = await axios.put("/user/setExercisesDone/" + username, {
-      exercisesDone,
-    });
+    const response = await axiosClient.put(
+      "/user/setExercisesDone/" + username,
+      {
+        exercisesDone,
+      }
+    );
     // console.log(response.data);
     // console.log(response.status);
     return { data: response.data, status: response.status };
@@ -288,7 +298,7 @@ const setExercisesDoneDB = async (username, exercisesDone) => {
 // Adds like to post on server
 const addLikeToPost = async (postId, userId) => {
   try {
-    const response = await axios.post("/posts/like/" + postId, {
+    const response = await axiosClient.post("/posts/like/" + postId, {
       userId,
     });
     // console.log(response.data);
@@ -303,7 +313,7 @@ const addLikeToPost = async (postId, userId) => {
 // Deletes like from post on server
 const deleteLikeFromPost = async (postId, userId) => {
   try {
-    const response = await axios.delete("/posts/like/" + postId, {
+    const response = await axiosClient.delete("/posts/like/" + postId, {
       data: {
         userId,
       },
@@ -320,7 +330,7 @@ const deleteLikeFromPost = async (postId, userId) => {
 // Adds comment to post on server
 const addCommentToPost = async (postId, userId, comment) => {
   try {
-    const response = await axios.post("/posts/comment/" + postId, {
+    const response = await axiosClient.post("/posts/comment/" + postId, {
       userId,
       comment,
     });
@@ -336,7 +346,7 @@ const addCommentToPost = async (postId, userId, comment) => {
 // Deletes comment from post on server
 const deleteCommentFromPost = async (postId, commentId) => {
   try {
-    const response = await axios.delete(
+    const response = await axiosClient.delete(
       "/posts/" + postId + "/comment/" + commentId
     );
     // console.log(response.data);
@@ -351,7 +361,7 @@ const deleteCommentFromPost = async (postId, commentId) => {
 // Deletes test posts from server
 const deleteTestPosts = async () => {
   try {
-    const response = await axios.delete("/testPosts");
+    const response = await axiosClient.delete("/testPosts");
     // console.log(response.data);
     // console.log(response.status);
     return { data: response.data, status: response.status };
@@ -362,7 +372,7 @@ const deleteTestPosts = async () => {
 
 const deleteEvent = async (eventId) => {
   try {
-    const response = await axios.delete("/eventPosts/" + eventId);
+    const response = await axiosClient.delete("/eventPosts/" + eventId);
     return { data: response.data, status: response.status };
   } catch (error) {
     // console.log(error);
@@ -371,7 +381,7 @@ const deleteEvent = async (eventId) => {
 
 const getPartnerFindEntries = async () => {
   try {
-    const response = await axios.get("/partnerEntry");
+    const response = await axiosClient.get("/partnerEntry");
     return { data: response.data, status: response.status };
   } catch (error) {
     // console.log(error);
@@ -380,7 +390,7 @@ const getPartnerFindEntries = async () => {
 
 const createPartnerFindEntry = async (entry) => {
   try {
-    const response = await axios.post("/partnerEntry", entry);
+    const response = await axiosClient.post("/partnerEntry", entry);
     return { data: response.data, status: response.status };
   } catch (error) {
     // console.log(error);
@@ -389,9 +399,12 @@ const createPartnerFindEntry = async (entry) => {
 
 const addInterestToPartnerFindEntry = async (entryId, userId) => {
   try {
-    const response = await axios.post("/partnerEntry/interest/" + entryId, {
-      userId,
-    });
+    const response = await axiosClient.post(
+      "/partnerEntry/interest/" + entryId,
+      {
+        userId,
+      }
+    );
     return { data: response.data, status: response.status };
   } catch (error) {
     // console.log(error);
@@ -400,11 +413,14 @@ const addInterestToPartnerFindEntry = async (entryId, userId) => {
 
 const deleteInterestFromPartnerFindEntry = async (entryId, userId) => {
   try {
-    const response = await axios.delete("/partnerEntry/interest/" + entryId, {
-      data: {
-        userId,
-      },
-    });
+    const response = await axiosClient.delete(
+      "/partnerEntry/interest/" + entryId,
+      {
+        data: {
+          userId,
+        },
+      }
+    );
     return { data: response.data, status: response.status };
   } catch (error) {
     // console.log(error);
@@ -413,7 +429,7 @@ const deleteInterestFromPartnerFindEntry = async (entryId, userId) => {
 
 const deletePartnerFindEntry = async (entryId) => {
   try {
-    const response = await axios.delete("/partnerEntry/" + entryId);
+    const response = await axiosClient.delete("/partnerEntry/" + entryId);
     return { data: response.data, status: response.status };
   } catch (error) {
     // console.log(error);
@@ -422,7 +438,7 @@ const deletePartnerFindEntry = async (entryId) => {
 
 const getSingleEntry = async (entryId) => {
   try {
-    const response = await axios.get("/partnerEntry/" + entryId);
+    const response = await axiosClient.get("/partnerEntry/" + entryId);
     return { data: response.data, status: response.status };
   } catch (error) {
     // console.log(error);
@@ -431,7 +447,7 @@ const getSingleEntry = async (entryId) => {
 
 const updateEntry = async (entry) => {
   try {
-    const response = await axios.put("/partnerEntry/" + entry._id, entry);
+    const response = await axiosClient.put("/partnerEntry/" + entry._id, entry);
     return { data: response.data, status: response.status };
   } catch (error) {
     // console.log(error);
